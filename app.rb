52dec84b71d00db56773c16a18fce58a343d8fcc
Anna <anna.voitik@gmail.com>
2020-05-11 16:00:25 +0100

    Form to enter a name

--- app.rb
+++ app.rb
@@ -8,7 +8,13 @@ get '/secret' do
   "This is a new secret message"
 end
 
-get '/cat' do
+get '/random-cat' do
  @name = ["Amigo", "Oscar", "Viking"].sample
  erb(:index)
 end
+
+get '/named-cat' do
+  p params 
+  @name = params[:name]
+  erb(:index)
+ end
